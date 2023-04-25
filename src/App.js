42d3f3e80d651b2b0c8bf530a41530ff13693a78
karenadloyan2000 <62433934+karenadloyan2000@users.noreply.github.com>
2023-04-25 20:55:32 +0400

    reouter

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
 
   return (
       <>
-        <BrowserRouter>
+        <BrowserRouter basename=“/Shop”>
           <MyRouter></MyRouter>
         </BrowserRouter>
         <MobileMneu></MobileMneu>
